c7f9193073b751b2e3c62eacdfff5e132b865443
Pokatilov-s <semenpokatilov465@gmail.com>
2024-04-22 16:57:51 +0700

    Update course create

--- forge/serializers.py
+++ forge/serializers.py
@@ -7,7 +7,7 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = ('uuid', 'category', 'title', 'price', 'description', 'status', 'author_uuid', 'updated_at',
                   'students_qty', 'reviews_qty')
-        read_only_fields = ('updated_at', 'status', 'students_qty', 'reviews_qty')
+        read_only_fields = ('updated_at', 'status', 'students_qty', 'reviews_qty', 'author_uuid')
 
 
 
--- forge/views.py
+++ forge/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Category, Course
 from .serializers import CourseSerializer
 
@@ -6,4 +9,8 @@ from .serializers import CourseSerializer
 class ForgeCourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    authentication_classes = (TokenAuthentication, SessionAuthentication)
+    permission_classes = (IsAuthenticated,)
 
+    def perform_create(self, serializer):
+        serializer.save(author_uuid=self.request.user)
