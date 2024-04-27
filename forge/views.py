76e331abcf498c79cd086b1f1b945922f7893346
Pokatilov-s <semenpokatilov465@gmail.com>
2024-04-27 19:29:14 +0700

    Update API endpoint apps forge

--- forge/views.py
+++ forge/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Category, Course
+from .models import Course
 from .serializers import CourseSerializer
 
 
@@ -14,3 +14,9 @@ class ForgeCourseViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author_uuid=self.request.user)
+
+    def get_queryset(self):
+        user = self.request.user
+        """Используя обратную связь получаем все связанные с user объекты из таблицы Courses"""
+        queryset = Course.objects.filter(author_uuid=user)
+        return queryset
