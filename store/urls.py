4f7649603a05e5314b327c33d2df807b6bcd1189
Pokatilov-s <semenpokatilov465@gmail.com>
2024-04-27 21:02:52 +0700

    Update URLS

--- store/urls.py
+++ store/urls.py
@@ -2,12 +2,12 @@ from rest_framework import routers
 from django.urls import path, include
 from .views import CoursesReadOnlyViewSet, CategoriesViewSet, AddCourseToUser, CoursesAddedUser
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r'courses', CoursesReadOnlyViewSet, basename='courses')
 router.register(r'categories', CategoriesViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('addcourse/', AddCourseToUser.as_view(), name='add-course'),
-    path('mycourse/', CoursesAddedUser.as_view(), name='my-course'),
+    path('add_course/', AddCourseToUser.as_view(), name='add-course'),
+    path('my_course/', CoursesAddedUser.as_view(), name='my-course'),
 ]
