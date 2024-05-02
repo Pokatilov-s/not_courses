6092497df6ef7ecabdec8afe518cfdb157d38c41
Pokatilov-s <semenpokatilov465@gmail.com>
2024-05-02 13:38:42 +0700

    Update

--- forge/views.py
+++ forge/views.py
@@ -7,16 +7,19 @@ from .serializers import CourseSerializer
 
 
 class ForgeCourseViewSet(viewsets.ModelViewSet):
+    """API endpoint позволяющий создавать просматривать и редактировать курсы"""
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     authentication_classes = (TokenAuthentication, SessionAuthentication)
     permission_classes = (IsAuthenticated,)
 
     def perform_create(self, serializer):
+        """Создание курса с привязкой автора"""
         serializer.save(author_uuid=self.request.user)
 
     def get_queryset(self):
+        """Получение списка курсов принадлежащих автору"""
         user = self.request.user
-        """Используя обратную связь получаем все связанные с user объекты из таблицы Courses"""
+        # Используя обратную связь получаем все связанные с user объекты из таблицы Courses
         queryset = Course.objects.filter(author_uuid=user)
         return queryset
