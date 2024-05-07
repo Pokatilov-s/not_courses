d4e4a64f872d008019fcdee674596bb530f3b4c8
Pokatilov-s <semenpokatilov465@gmail.com>
2024-05-07 23:46:20 +0700

    Update get_queryset and crate retrieve ForgeCourseViewSet

--- forge/services.py
+++ forge/services.py
@@ -1,7 +1,8 @@
-from custom_user import models
-
-
-def get_list_courses_owned_by_author(user: models.User):
+def get_list_courses_owned_by_author(user, status=None):
     """Получить список курсов принадлежащих автору"""
     # Используя обратную связь course_set получаем все связанные с user объекты из таблицы Courses
+    # если передаётся status фильтруем по статусу
+    if status is not None:
+        return user.course_set.filter(status=status)
+
     return user.course_set
--- forge/views.py
+++ forge/views.py
@@ -1,14 +1,15 @@
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
-from .models import Course
+from rest_framework.response import Response
+
 from .serializers import CourseSerializer
 from .services import get_list_courses_owned_by_author
+from .models import Course
 
 
 class ForgeCourseViewSet(viewsets.ModelViewSet):
     """API endpoint позволяющий создавать, просматривать и редактировать курсы принадлежащие автору"""
-    # queryset = Course.objects.all()
     serializer_class = CourseSerializer
     authentication_classes = (TokenAuthentication, SessionAuthentication)
     permission_classes = (IsAuthenticated,)
@@ -19,8 +20,16 @@ class ForgeCourseViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         """Получение списка курсов принадлежащих автору"""
-        return get_list_courses_owned_by_author(self.request.user)
+        status = self.request.query_params.get('status')
+        return get_list_courses_owned_by_author(self.request.user, status=status)
 
     def retrieve(self, request, *args, **kwargs):
-        pass
-    """Необходимо переопределить все методы """
+        """Получение курса принадлежащего втору"""
+        # Используется метод retrieve родительского класса RetrieveModelMixin
+        # фильтрует полученный queryset из get_queryset что может быть не очень эффективно,
+        # но при этом выполняется условие валидации, автор может получить только принадлежащий ему курс.
+        # При необходимости оптимизации запросов добавить выше описанную валидацию.
+        return super().retrieve(request, *args, **kwargs)
+
+
+
