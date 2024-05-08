609341823d0a74cb8399a7102921f29f416f2b7f
Pokatilov-s <semenpokatilov465@gmail.com>
2024-05-08 23:30:09 +0700

    Update

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -16,8 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '192.168.1.51']
 
 # Application definition
 INSTALLED_APPS = [
