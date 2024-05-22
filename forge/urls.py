997334c31307b653597b352877ff95398eb632fe
Pokatilov-s <semenpokatilov465@gmail.com>
2024-05-22 23:59:29 +0700

    Update

--- forge/urls.py
+++ forge/urls.py
@@ -7,5 +7,6 @@ router.register(r'course', ForgeCourseViewSet, basename='course')
 
 urlpatterns = [
     path('status_update/<uuid:pk>/', UpdateCourseStatus.as_view(), name='status_update'),
+
 ]
 urlpatterns += router.urls
--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,7 @@
+import time
+from django.http import JsonResponse
 from django.shortcuts import render
-
-from forge.models import Category
+from django.views.decorators.csrf import csrf_exempt
 from store.services import get_list_published_courses, get_list_categories
 
 
@@ -13,3 +14,21 @@ def index_store(request):
 def course_detail(request, course_uuid):
     course = get_list_published_courses(course_uuid)
     return render(request, 'front/course_detail.html', {'course': course})
+
+
+# Платёжка
+def payment_page(request):
+    course_info = request.session.get('course_info', {})
+    return render(request, 'front/payment/payment.html', {'course_info': course_info})
+
+
+@csrf_exempt
+def process_payment(request):
+    if request.method == 'POST':
+        time.sleep(10)
+        return JsonResponse({'status': 'success', 'message': 'Платеж успешно обработан!'})
+    return JsonResponse({'status': 'error', 'message': 'Ошибка обработки платежа.'})
+
+
+def success_page(request):
+    return render(request, 'front/payment/success.html')
