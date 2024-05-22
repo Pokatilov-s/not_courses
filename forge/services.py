cb78a5320f9550d72434d34774c35c04883196ce
Pokatilov-s <semenpokatilov465@gmail.com>
2024-05-22 14:15:51 +0700

    Update

--- forge/services.py
+++ forge/services.py
@@ -12,5 +12,5 @@ def get_list_courses_owned_by_author(user, status=None):
 
 
 def get_courses_owned_by_author(user, pk):
+    """Получить экземпляр курса принадлежащий автору"""
     return get_object_or_404(user.course_set, pk=pk)
-
