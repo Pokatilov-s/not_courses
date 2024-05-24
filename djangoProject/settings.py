85c9efe3369945ec2b542d97bed4f6cbe9a21531
Pokatilov-s <semenpokatilov465@gmail.com>
2024-05-24 23:59:06 +0700

    Update

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -141,8 +141,8 @@ AUTH_USER_MODEL = 'custom_user.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
